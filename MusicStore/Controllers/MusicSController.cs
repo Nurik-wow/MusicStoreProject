8284aa67663453920c9ef5e4268708a7015b1bf9
Nurik-wow <60682311+Nurik-wow@users.noreply.github.com>
2020-11-23 00:59:25 +0600

    update

---
+++ MusicStore/Controllers/MusicSController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class MusicSController : Controller
+    {
+        // GET: MusicS
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+            return View(genres);
+        }
+
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        public ActionResult Descr(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+    
+    }
+}
